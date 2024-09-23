2f2e5c1a9c4e6452ac1e051b0a1e257ec7577d07
taissir alem <taissir.alem@gmail.com>
2024-09-23 16:32:31 +0100

    fixes

--- src/routes/v1/NewsletterRoutes.js
+++ src/routes/v1/NewsletterRoutes.js
@@ -1,9 +1,9 @@
 const { Router } = require("express");
 const NewsletterController = require("../../controllers/Newslettercontroller.js");
-const {adminAuthAndRoleCheck} = require("../../middlewares/authcheck.js");
+const { adminAuthAndRoleCheck } = require("../../middlewares/authcheck.js");
 const router = Router();
 
 router.get("/", adminAuthAndRoleCheck, NewsletterController.getAllNewsletters);
 router.post("/create", NewsletterController.createNewsletter);
-router.get("/count", NewsletterController.countAllNewsletters);
+router.get("/count", NewsletterController.getAllNewslettersCount);
 module.exports = router;
